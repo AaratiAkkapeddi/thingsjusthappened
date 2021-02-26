d08a39407978d7aeccfd3fdf503ea2e074515a8b
AaratiAkkapeddi <aarati.akkapeddi@gmail.com>
2021-02-25 20:03:19 -0500

    starting to setup individual project page data

---
+++ homepage.js
@@ -0,0 +1,42 @@
+            
+      var public_spreadsheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQB54w_pHd6z8PNNXxe1mEVNOFd2bqG5mhPcfLQXcjj_h2UisWJXqKUfe_ySlHN_1xJOA6i8GD_lU2-/pub?gid=1129457337&single=true&output=csv"
+      var slug = window.location.href.split('project=')[1];
+      function init() {
+        Papa.parse(public_spreadsheet_url, {
+          download: true,
+          header: true,
+          complete: showInfo
+        })
+      }
+
+      if(slug){
+
+       window.addEventListener('DOMContentLoaded', init)
+      }
+   
+
+      
+
+      function showInfo(results) {
+        var data = results.data
+        console.log(slug)
+
+        for (var i = data.length - 1; i >= 0; i--) {
+          let project = document.createElement("div");
+          project.classList.add("project");
+          if(data[i]["Images"].length > 0){
+            let thumbnail = document.createElement("img");
+            thumbnail.src = data[i]["Images"].split(",")[0]
+            thumbnail.src = thumbnail.src.split("/open?").join("/uc?")
+
+            project.append(thumbnail)
+          }
+          
+          // p.innerHTML = data[i].text
+          console.log(data[i]["Images"])
+          document.getElementById("data-area").append(project);
+        }
+
+
+      }
+
